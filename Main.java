2bbe58102bce73bca969f9cdd45784a30ee4ea58
carmichael901230 <34822412+carmichael901230@users.noreply.github.com>
2019-03-03 21:22:54 -0500

    Challenge 3 added
    find values of B and C which make 0<m<1, -1<cp<1 when T=0

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.concurrent.*;
 import java.util.Scanner;
 
 class Main {
@@ -7,7 +8,7 @@ class Main {
     public static void main(String[] args) {
         // ===== Part 1 =====
         // Automatically determine value of Nf and Nm
-        Part1();        
+        // Part1();        
 
         // ===== Part 2 =====
         // Input B, C, T. Compute <cp> and <m> 
@@ -24,6 +25,11 @@ class Main {
         // Automatically find value of B and C, 
         // and compute <m> with respect of different value of T
         // Part4();
+
+        // ===== Challenge 3 =====
+        // Find value of B and C, letting 0<m<1 and −1<cp<1 at T = 0
+        Challenge3();
+
     }
 
     public static void Part1() {
@@ -56,6 +62,49 @@ class Main {
     public static void Part4() {
         double[] B_C = determineBC_cp();      // determine B and C
         findMin(B_C[0], B_C[1]);       // find peak using given B, C values
+    }
+
+    public static void Challenge3() {
+        System.out.println("Seeking For Value of B, C...");
+        double B=0.1, C=-0.1, T=0.01;
+        int N = 100;
+        int Nt = 1000;
+        // use small Nm and Nf to speed up performace
+        int Nm = 20;        
+        int Nf = 5;
+        outerloop: 
+        for (; B<=2; B+=0.1) {
+            for (; C>=-2; C-=0.1) {
+                ArrayList<Mthread> threadList = new ArrayList<>();
+                for (int i=0; i<Nt; i++) {
+                    threadList.add(new Mthread(N, B, C, T, Nm, Nt, Nf, i));
+                }
+                for (int i=0; i<Nt; i++) {
+                    threadList.get(i).start();
+                }
+        
+                try {
+                    Mthread.finished.acquire();
+                    double m = threadList.get(0).globalMeanM;
+                    double cp = threadList.get(0).globalMeanC;
+                    // System.out.println("B="+B+" C="+C +" <m>:"+m+" <cp>=" +cp );
+                    if (m>0.01 && m <0.99 && cp>-0.99 && cp<0.99) {
+                        System.out.println("Valid Value Found");
+                        System.out.println("B="+B+" C="+C+" <m>:"+m+" <cp>=" +cp );
+                        threadList.clear();
+                        Mthread.globalC.clear();
+                        Mthread.globalM.clear();
+                        break outerloop;
+                    }
+                }catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
+                threadList.clear();
+                Mthread.globalC.clear();
+                Mthread.globalM.clear();
+            }
+            C=-0.3;        // reset C after inner loop
+        }
     }
 
     public static void computeMeans(double B,double C, double T,int Nm, int Nf) {
@@ -180,14 +229,15 @@ class Main {
         return Nm;
     }
 
+    // determine a single pair of B_C
     public static double[] determineBC_m() {
         System.out.println("Seeking For Value of B, C, Making <m>=0...");
         double B=0.5, C=-0.001, T=0.01;
         int N = 100;
         int Nt = 1000;
         // use small Nm and Nf to speed up performace
-        int Nm = 50;        
-        int Nf = 8;
+        int Nm = 20;        
+        int Nf = 5;
         outerloop: 
         for (; B<=2.5; B+=0.1) {
             for (; C>=-2.5; C-=0.1) {
@@ -201,7 +251,7 @@ class Main {
         
                 try {
                     Mthread.finished.acquire();
-                    // System.out.println("B="+B+" C="+C+" <m>=" + threadList.get(0).globalMeanM);
+                    System.out.println("B="+B+" C="+C+" <m>=" + threadList.get(0).globalMeanM);
                     if (Math.abs(threadList.get(Nt-1).globalMeanM)<=1E-10) {
                         System.out.println("Valid Value Found");
                         System.out.println("B="+B+" C="+C+" <m>=" + threadList.get(0).globalMeanM);
@@ -223,15 +273,16 @@ class Main {
         return result;
     }
 
+
     public static void findPeak(double B, double C) {
         double T=0.01;
         int N = 100;
         int Nt = 1000;
         // use small Nm and Nf to speed up performace
-        int Nm = 50;        
-        int Nf = 8;
-        System.out.println("Exihibiting Peak at T>0 ...");
-        for (; T<2; T+=0.08) {
+        int Nm = 20;        
+        int Nf = 5;
+        // System.out.println("Exihibiting Peak at T>0 ...");
+        for (; T<4; T+=0.08) {
             ArrayList<Mthread> threadList = new ArrayList<>();
             for (int i=0; i<Nt; i++) {
                 threadList.add(new Mthread(N, B, C, T, Nm, Nt, Nf, i));
@@ -251,6 +302,7 @@ class Main {
             Mthread.globalM.clear();
         }
     }
+
 
     public static double[] determineBC_cp() {
         System.out.println("Seeking For Value of B, C. Making <cp>=1...");
